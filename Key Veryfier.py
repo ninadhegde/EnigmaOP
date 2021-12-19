985b2adf6e044e09d41b9782e87037547923cff4
ninadhegde <91817083+ninadhegde@users.noreply.github.com>
2021-12-19 12:57:11 +0530

    changes made
    Removed class and made it as it was before

--- Key Veryfier.py
+++ Key Veryfier.py
@@ -5,10 +5,12 @@ lst2x=[0]*128
 lst2x=random.sample(range(0, 127), 120)
 plugs=[-1]*128
 x=1
-for i in plugs:
+for i in lst2x:
     if i!=plugs[i]:
         x=0
 if x==0:
     print('Plug Failed')
 else:
     print('Success')
+    print(plugs)
+    print(lst2x)

db77f81283acab1cea766d39845fe3dcd4a7c209
ninadhegde <91817083+ninadhegde@users.noreply.github.com>
2021-12-21 13:35:42 +0530

    code update
    took initial setting variable as tuple datatype but still not decrypt working

--- EnigmaOP.py
+++ EnigmaOP.py
@@ -18,15 +18,15 @@ ascii_num=12
 input_string=input('Enter Something for encryption')
 copyinput=input_string
 cipher_text=''
-initial_settings=[]
+initialsettings=[]
 plugboard={}
 
 
 Rotor_combination,Rotor_Setting,plugboard=generate.random_settings()
-initial_settings.append(Rotor_combination)
-initial_settings.append(Rotor_Setting)
-initial_settings.append(plugboard)
-
+initialsettings.append(Rotor_combination)
+initialsettings.append(Rotor_Setting)
+initialsettings.append(plugboard)
+initial_settings=tuple(initialsettings)
 for i in input_string:
     ascii_num=convert.tonum(i)
     print(ascii_num)
